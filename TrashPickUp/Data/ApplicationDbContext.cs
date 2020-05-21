9eac8e1740417cf504afa8bfed7f97627025275e
Jeff <jeffrey.r.carter86@gmail.com>
2020-05-21 09:36:44 -0400

    setup migrations and database

--- TrashPickUp/Data/ApplicationDbContext.cs
+++ TrashPickUp/Data/ApplicationDbContext.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TrashPickUp.Models;
 
 namespace TrashPickUp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                new IdentityRole
+                {
+                    Name = "Customer",
+                    NormalizedName = "Customer"
+                },
+                new IdentityRole
+                {
+                    Name = "Employee",
+                    NormalizedName = "Employee"
+                });
         }
     }
 }
